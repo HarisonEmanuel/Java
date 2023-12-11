b0b4bfcd8c6c8e8edb22196fbb5bfb5c9a72213a
HarisonEmanuel <142061751+HarisonEmanuel@users.noreply.github.com>
2023-12-11 00:20:05 -0400

    Atualizar o BuscaNome.java

--- BuscaNome.java
+++ BuscaNome.java
@@ -2,17 +2,21 @@ import java.util.Scanner;
 
 public class BuscaNome {
     public static void main(String[] args) {
+        // Inicializando um array para armazenar 10 nomes
         String[] nomes = new String[10];
         Scanner scanner = new Scanner(System.in);
 
+        // Preenchendo o array com nomes fornecidos pelo usuário
         for (int i = 0; i < 10; i++) {
             System.out.print("Digite o nome #" + (i + 1) + ": ");
             nomes[i] = scanner.nextLine();
         }
 
+        // Solicitando ao usuário um nome para buscar
         System.out.print("Digite um nome para buscar: ");
         String nomeBusca = scanner.nextLine();
 
+        // Verificando se o nome fornecido está no array
         boolean encontrado = false;
         for (String nome : nomes) {
             if (nome.equalsIgnoreCase(nomeBusca)) {
@@ -21,12 +25,14 @@ public class BuscaNome {
             }
         }
 
+        // Exibindo o resultado da busca
         if (encontrado) {
             System.out.println("ACHEI");
         } else {
             System.out.println("NÃO ACHEI");
         }
 
+        // Fechando o scanner para evitar vazamento de recursos
         scanner.close();
     }
 }
