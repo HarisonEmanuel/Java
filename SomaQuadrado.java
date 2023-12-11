fb5fc96474371e70e70976357af337c9cc51f54e
HarisonEmanuel <142061751+HarisonEmanuel@users.noreply.github.com>
2023-12-11 00:56:22 -0400

    Atualizar o SomaQuadrado.java

--- SomaQuadrado.java
+++ SomaQuadrado.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class SomaQuadrado {
-    public static void main(String[] args) {
+    public static main(String[] args) {
         Scanner input = new Scanner(System.in);
 
         System.out.print("Digite o valor de A: ");
@@ -13,7 +13,10 @@ public class SomaQuadrado {
         System.out.print("Digite o valor de C: ");
         double c = input.nextDouble();
 
+        // Calcula a soma dos valores
         double soma = a + b + c;
+
+        // Calcula o quadrado da soma
         double resultado = Math.pow(soma, 2);
 
         System.out.println("O quadrado da soma dos valores é: " + resultado);
