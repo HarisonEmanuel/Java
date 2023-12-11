5ff10174604c813324bd0673163635025b68582d
HarisonEmanuel <142061751+HarisonEmanuel@users.noreply.github.com>
2023-12-11 00:58:09 -0400

    Atualizar o SomaValoresPares.java

--- SomaValoresPares.java
+++ SomaValoresPares.java
@@ -2,9 +2,10 @@ public class SomaValoresPares {
     public static void main(String[] args) {
         int soma = 0;
 
+        // Loop de 1 a 500 para encontrar e somar valores pares
         for (int i = 1; i <= 500; i++) {
-            if (i % 2 == 0) {
-                soma += i;
+            if (i % 2 == 0) {  // Verifica se o número é par
+                soma += i;      // Adiciona o valor par à soma
             }
         }
 
