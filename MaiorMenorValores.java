f29bb13d6e3c4e1844fe3b2d21eec3a4052fa894
HarisonEmanuel <142061751+HarisonEmanuel@users.noreply.github.com>
2023-12-11 00:39:18 -0400

    Atualizar o MaiorMenorValores.java

--- MaiorMenorValores.java
+++ MaiorMenorValores.java
@@ -4,8 +4,8 @@ public class MaiorMenorValores {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int maior = Integer.MIN_VALUE;
-        int menor = Integer.MAX_VALUE; 
+        int maior = Integer.MIN_VALUE; // Variável para armazenar o maior valor inserido.
+        int menor = Integer.MAX_VALUE; // Variável para armazenar o menor valor inserido.
 
         int valor;
 
@@ -14,15 +14,15 @@ public class MaiorMenorValores {
             valor = scanner.nextInt();
 
             if (valor < 0) {
-                break; 
+                break; // Encerra o loop quando um valor negativo é inserido.
             }
 
             if (valor > maior) {
-                maior = valor;
+                maior = valor; // Atualiza o maior valor se o valor atual for maior.
             }
 
             if (valor < menor) {
-                menor = valor;
+                menor = valor; // Atualiza o menor valor se o valor atual for menor.
             }
         }
 
@@ -30,7 +30,7 @@ public class MaiorMenorValores {
             System.out.println("Maior valor informado: " + maior);
             System.out.println("Menor valor informado: " + menor);
         } else {
-            System.out.println("Nenhum valor positivo foi informado.");
+            System.out.println("Nenhum valor positivo foi informado."); // Exibido se nenhum valor positivo for inserido.
         }
 
         scanner.close();
