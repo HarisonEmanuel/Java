d118b13bfd2e3cd233e5a331f94d6396ff0d90dc
HarisonEmanuel <142061751+HarisonEmanuel@users.noreply.github.com>
2023-12-11 00:35:43 -0400

    Atualizar o InsertionSort.java

--- InsertionSort.java
+++ InsertionSort.java
@@ -1,10 +1,13 @@
 public class InsertionSort {
+
+    // Método para ordenar um array usando o algoritmo Insertion Sort.
     public static void insertionSort(int[] array) {
         int n = array.length;
         for (int i = 1; i < n; ++i) {
             int key = array[i];
             int j = i - 1;
 
+            // Move os elementos maiores que a chave para a direita.
             while (j >= 0 && array[j] > key) {
                 array[j + 1] = array[j];
                 j = j - 1;
@@ -13,6 +16,7 @@ public class InsertionSort {
         }
     }
 
+    // Método para imprimir os elementos de um array.
     public static void printArray(int[] array) {
         int n = array.length;
         for (int i = 0; i < n; ++i) {
@@ -26,6 +30,7 @@ public class InsertionSort {
         System.out.println("Array original:");
         printArray(array);
 
+        // Chama o método insertionSort para ordenar o array.
         insertionSort(array);
 
         System.out.println("Array ordenado:");
