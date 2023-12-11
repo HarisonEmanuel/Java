64d70c7d5173247c573f61eb54a6f89259bb24c9
HarisonEmanuel <142061751+HarisonEmanuel@users.noreply.github.com>
2023-12-11 00:50:07 -0400

    Atualizar o NumerosImpares.java

--- NumerosImpares.java
+++ NumerosImpares.java
@@ -1,7 +1,10 @@
 public class NumerosImpares {
     public static void main(String[] args) {
+        // Itera de 0 a 20
         for (int i = 0; i <= 20; i++) {
-            if (i % 2 != 0) { 
+            // Verifica se o número é ímpar
+            if (i % 2 != 0) {
+                // Imprime números ímpares
                 System.out.println(i);
             }
         }
